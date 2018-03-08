c99f2d43bbbd68aa6cad76a3a26a395c4543cedf
Yunkai Feng <yunkaifeng2009@gmail.com>
2018-03-08 17:07:06 +0800

    Add a uuid test file

---
+++ uuid-test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+)
+
+func main() {
+	// Creating UUID Version 4
+	// panic on error
+	u1 := uuid.Must(uuid.NewV4())
+	fmt.Printf("UUIDv4: %s\n", u1)
+
+	// or error handling
+	u2, err := uuid.NewV4()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return
+	}
+	fmt.Printf("UUIDv4: %s\n", u2)
+
+}
